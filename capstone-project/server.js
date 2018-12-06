1bfa7feaece757ff66e7e8e05c1dfca34247277e
dennomwas <githinji.mwangi@gmail.com>
2018-12-06 14:15:29 +0300

    [Ch] Refactor server.js file
    - Refactor folder structure

--- capstone-project/server.js
+++ capstone-project/server.js
@@ -5,11 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/capstone-project'));
+app.use(express.static(__dirname + '/dist/capstone-angular-frontend'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname, 'dist', 'capstone-project', 'index.html'));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname+'/dist/capstone-project/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
